a5e604630ee3b132b5e6c2fd08906d83ff52b11a
Ben Lovell <ben.j.lovell@gmail.com>
2026-01-27 04:36:11 +0100

    Make port configurable

--- src/main.rs
+++ src/main.rs
@@ -930,7 +930,11 @@ async fn main() {
 
     let app = create_app();
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let port: u16 = std::env::var("PORT")
+        .ok()
+        .and_then(|p| p.parse().ok())
+        .unwrap_or(3000);
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
     tracing::info!("listening on {}", addr);
 
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
