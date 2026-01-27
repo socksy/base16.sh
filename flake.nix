ba25f4a1451c78e29756c4d20b40bcfc4a909bbd
Ben Lovell <ben.j.lovell@gmail.com>
2026-01-27 05:22:55 +0100

    Make flake work on macOS

--- flake.nix
+++ flake.nix
@@ -2,21 +2,28 @@
   description = "Base16/Base24 color scheme server";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
     in
     {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "base16-server";
-        version = "0.1.0";
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-      };
+      packages = forAllSystems (pkgs: {
+        default = pkgs.rustPlatform.buildRustPackage {
+          pname = "base16-server";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+      });
 
       nixosModules.default = { pkgs, ... }: {
         systemd.services.base16-server = {
