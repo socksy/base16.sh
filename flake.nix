78c0aaa5991d25bfd3d05c39f5ff5f5f91c4d674
Ben Lovell <ben.j.lovell@gmail.com>
2026-01-28 02:09:32 +0100

    Make schemes available from nix flake

--- flake.nix
+++ flake.nix
@@ -3,9 +3,13 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    schemes = {
+      url = "github:tinted-theming/schemes";
+      flake = false;
+    };
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, schemes }:
     let
       systems = [
         "x86_64-linux"
@@ -22,20 +26,33 @@
           version = "0.1.0";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
+
+          postInstall = ''
+            mkdir -p $out/share/base16-server/data
+            ln -s ${schemes} $out/share/base16-server/data/schemes
+          '';
         };
       });
 
-      nixosModules.default = { pkgs, ... }: {
-        systemd.services.base16-server = {
-          wantedBy = [ "multi-user.target" ];
-          serviceConfig = {
-            ExecStart = "${self.packages.${pkgs.system}.default}/bin/base16-server";
-            WorkingDirectory = "${self}";
-            Restart = "always";
-            DynamicUser = true;
+      nixosModules.default = { pkgs, config, lib, ... }:
+        let
+          cfg = config.services.base16-server;
+        in
+        {
+          options.services.base16-server.enable = lib.mkEnableOption "base16-server";
+
+          config = lib.mkIf cfg.enable {
+            systemd.services.base16-server = {
+              wantedBy = [ "multi-user.target" ];
+              serviceConfig = {
+                ExecStart = "${self.packages.${pkgs.system}.default}/bin/base16-server";
+                WorkingDirectory = "${self.packages.${pkgs.system}.default}/share/base16-server";
+                Restart = "always";
+                DynamicUser = true;
+              };
+              environment.RUST_LOG = "info";
+            };
           };
-          environment.RUST_LOG = "info";
         };
-      };
     };
 }
